e8824fa4e5d4738d189c43125505999026268de9
Pooja Gawade <104082200+OKPooja@users.noreply.github.com>
2023-06-08 17:37:49 +0530

    Add files via upload

---
+++ chat_app/chat/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, redirect 
+from . models import Room, Msg
+from django.http import HttpResponse, JsonResponse
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def room(request, room):
+    username = request.GET.get('username')
+    room_details = Room.objects.get(name = room)
+
+    data = {
+        'username' : username,
+        'room' : room, 
+        'room_details' : room_details
+    }
+
+    return render(request, 'room.html', data)
+
+
+def checkview(request):
+    room = request.POST['room_name']
+    username = request.POST['username']
+
+    if Room.objects.filter(name= room).exists():
+        return redirect('/' + room + '/?username=' + username)
+    else:
+        new_room = Room.objects.create(name= room)
+        new_room.save()
+        return redirect('/' + room + '/?username=' + username)
+
+def send(request):
+    message = request.POST['message']     
+    username = request.POST['username']     
+    room_id = request.POST['room_id']   
+
+    new_msg = Msg.objects.create(value= message, user= username,room= room_id)
+    new_msg.save()
+
+    return HttpResponse('Message sent successfully')  
+
+def getMessages(request, room):
+    
+    room_details = Room.objects.get(name= room)
+    messages = Msg.objects.filter(room= room_details.id)
+    
+    return JsonResponse({'messages' : list(messages.values())})
+
+    
